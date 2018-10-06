f0c0dcad7838dd4ea724dc89f7954b024897d0d0
Andrew Brainerd <drwb333@gmail.com>
2018-10-06 10:54:53 -0400

    Remove logs

--- server.js
+++ server.js
@@ -13,16 +13,11 @@ app.use(function(req, res, next) {
   });
 
 app.get('/api/profile/:userId', (req, res) => {
-    console.log('GET /api/profile/' + req.params.userId);
-
     const platform = 'pc';
     const region = 'us';
     const tag = req.params.userId;
     
     overwatch.getProfile(platform, region, tag, (profileJson) => {
-        console.log('Profile Info for ' + tag);
-        console.log(JSON.stringify(profileJson, null, 2));
-
         res.send({ 
             info: profileJson,
             message: 'Got Profile!'
@@ -31,16 +26,11 @@ app.get('/api/profile/:userId', (req, res) => {
 });
 
 app.get('/api/stats/:userId', (req, res) => {
-    console.log('GET /api/stats/' + req.params.userId);
-
     const platform = 'pc';
     const region = 'us';
     const tag = req.params.userId;
     
     overwatch.getStats(platform, region, tag, (statsJson) => {
-        console.log('Stats for ' + tag);
-        console.log(JSON.stringify(statsJson.stats.top_heroes, null, 2));
-        
         res.send({ 
             username: statsJson.username,
             level: statsJson.level,

97d84b84728c4a1fa63409b4cfb49ca7e4761556
Andrew Brainerd <drwb333@gmail.com>
2019-01-21 18:02:58 -0500

    Update overwatch-api version

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').load();
 const express = require('express');
 const bodyParser = require('body-parser');
 const overwatch = require('overwatch-api');
@@ -6,7 +5,7 @@ const overwatch = require('overwatch-api');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -26,30 +25,61 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/api/profile/:userId', (req, res) => {
+    console.log(`GET /api/profile/${req.params.userId}`);
+
     const platform = 'pc';
     const region = 'us';
     const tag = req.params.userId;
 
-    overwatch.getProfile(platform, region, tag, (profileJson) => {
-        res.send({
-            info: profileJson,
-            message: 'Got Profile!'
-        });
+    overwatch.getProfile(platform, region, tag, (err, profileJson) => {
+        if (!err) {
+            res.send({
+                info: profileJson,
+                message: 'Got Profile!'
+            });
+        } else {
+            console.log(err);
+        }
     });
 });
 
 app.get('/api/stats/:userId', (req, res) => {
+    console.log(`GET /api/stats/${req.params.userId}`);
+
+    const platform = 'pc';
+    const region = 'us';
+    const tag = req.params.userId;
+
+    overwatch.getStats(platform, region, tag, (err, statsJson) => {
+        console.log(`Stats for ${tag}`);
+        console.log(JSON.stringify(statsJson, null, 2));
+
+        if (!err) {
+            res.send({
+                stats: statsJson && statsJson.stats,
+                combat: statsJson && statsJson.stats && statsJson.stats.combat,
+                message: 'Got Stats!'
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/api/stats/:userId/combat', (req, res) => {
+    console.log(`GET /api/stats/${req.params.userId}/combat`);
+
     const platform = 'pc';
     const region = 'us';
     const tag = req.params.userId;
 
     overwatch.getStats(platform, region, tag, (statsJson) => {
+        console.log(`Stats for ${tag}`);
+        console.log(JSON.stringify(statsJson.stats, null, 2));
+
         res.send({
-            username: statsJson.username,
-            level: statsJson.level,
-            portrait: statsJson.portrait,
-            stats: statsJson.stats,
-            message: 'Got Stats!'
+            stats: statsJson.stats.combat,
+            message: 'Got Combat Stats!'
         });
     });
 });
